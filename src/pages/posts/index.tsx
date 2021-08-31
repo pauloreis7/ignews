c0f6d253bf40e78479c392647e7d7a492e007eb3
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-08-31 13:14:00 -0300

    Add post page structure and dynamics

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,12 @@
 import { GetStaticProps } from "next";
-import { Head } from "next/document";
+import Head from 'next/head'
 import Prismic from '@prismicio/client'
 import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from "../../services/prismic";
 
 import styles from './styles.module.scss'
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -18,7 +19,7 @@ interface PostsProps {
   posts: Post[];
 }
 
-export function Posts({ posts }: PostsProps) {
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -28,11 +29,13 @@ export function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           { posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-          </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           )) }
         </div>
       </main>
