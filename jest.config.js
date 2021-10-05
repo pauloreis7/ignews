4eed6cdd8ae60554d1f7d7f569aa47e456e5f6f2
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-10-05 09:54:25 -0300

    Fix tests config bugs

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   setupFilesAfterEnv: [
     "<rootDir>/src/tests/setupTests.ts"
   ],
-  trasform: {
+  transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
   testEnvironment: 'jsdom'
